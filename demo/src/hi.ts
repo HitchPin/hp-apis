fd0c20455ab2be7a8e2957aec15636fcf2e89d0f
Johnathan <johnathan@hitchpin.com>
2025-03-03 00:11:21 -0600

    Updated

--- demo/src/hi.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-
-export const Hi1 = z.object({ "asdf": z.string().optional(), "3tg": z.number().optional() })
-export const Hi2 = z.object({ "hello": z.string().optional(), "name": z.boolean().optional() })
-
-const hI3 = z.union([ Hi1, Hi2 ]);
-type HI = z.infer<typeof hI3>;
-type Hi2 = HI
